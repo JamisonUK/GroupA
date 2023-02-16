533ea213e7a29aa31f4fc3432069897763485409
Elvis-Jagger-Abdul-Jabbar <98457945+Elvis-Jagger-Abdul-Jabbar@users.noreply.github.com>
2023-02-16 10:36:48 +0000

    Update data-test.py

--- airflow/dags/data-test.py
+++ airflow/dags/data-test.py
@@ -46,7 +46,7 @@ def ProcessEmployees():
     @taskgit 
     def get_data():
         # NOTE: configure this as appropriate for your airflow environment
-        data_path = "/opt/airflow/dags/files/employees.csv"
+        data_path = "/opt/airflow/data/employees.csv"
         os.makedirs(os.path.dirname(data_path), exist_ok=True)
 
         url = "https://raw.githubusercontent.com/apache/airflow/main/docs/apache-airflow/tutorial/pipeline_example.csv"
